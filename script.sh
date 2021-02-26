86e727e1ef238a6d86c294987307fe330ef6c96c
Rishab Manocha <rishab.manocha@outlook.com>
2021-02-25 18:53:51 -0800

    Moved plex to MediaServer

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-HOST_DIR=$HOST_DIR/
-HOST_USER=mrishab
+export HOST_DIR=/mnt/external_hdd
+export HOST_USER=mrishab
 
 # Configure Jenkins folder Permissions
 mkdir -p $HOST_DIR/jenkins/
@@ -34,13 +34,25 @@ rm -rf $HOST_DIR/airflow/app/logs/*
 chown -R 50000:50000 $HOST_DIR/airflow/app/
 
 # MediaServer
-mkdir -p $HOST_DIR/mediaserver/downloads/torrents
-rm -rf $HOST_DIR/mediaserver/downloads/torrents/*
+mkdir -p $HOST_DIR/mediaserver/downloads/torrent
+rm -rf $HOST_DIR/mediaserver/downloads/torrent/*
 
-mkdir -p $HOST_DIR/mediaserver/downloads/movies
-rm -rf $HOST_DIR/mediaserver/downloads/movies/*
+mkdir -p $HOST_DIR/mediaserver/downloads/completed
+rm -rf $HOST_DIR/mediaserver/downloads/completed/*
 
-mkdir -p $HOST_DIR/mediaserver/downloads/tv
-rm -rf $HOST_DIR/mediaserver/downloads/tv/*
+mkdir -p $HOST_DIR/mediaserver/downloads/completed/Movies
+rm -rf $HOST_DIR/mediaserver/downloads/completed/Movies/*
+
+mkdir -p $HOST_DIR/mediaserver/movies
+rm -rf $HOST_DIR/mediaserver/movies/*
+
+mkdir -p $HOST_DIR/mediaserver/tv
+rm -rf $HOST_DIR/mediaserver/tv/*
+
+mkdir -p $HOST_DIR/mediaserver/plex/config
+rm -rf $HOST_DIR/mediaserver/plex/config/*
+
+mkdir -p $HOST_DIR/mediaserver/plex/transcode
+rm -rf $HOST_DIR/mediaserver/plex/transcode/*
 
 chown -R 201:201 $HOST_DIR/mediaserver/
