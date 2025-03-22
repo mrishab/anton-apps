e51de83b12984bb86ac43322bd1102e1cde0e174
Rishab Manocha <rishab.manocha@outlook.com>
2025-03-22 15:09:12 -0700

    added script to override the version history period

--- affine/scripts.sh
+++ affine/scripts.sh
@@ -5,8 +5,18 @@ docker exec -ti affine_postgres psql -U affine
 
 ```
 
+## Override the member-limit of 3 for all users:
+
 ```sql
 UPDATE features 
 SET configs = jsonb_set(configs::jsonb, '{memberLimit}', '10')
 WHERE configs::jsonb ? 'memberLimit';
+```
+
+## Override the document history-period to 365 days for all plans
+
+```sql
+UPDATE features
+SET configs = jsonb_set(configs::jsonb, '{historyPeriod}', '31536000000')
+WHERE configs::jsonb ? 'historyPeriod';
 ```
