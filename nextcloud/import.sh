b83ddacd71884fdc6a1bfcc318416ebdf4b478f1
Your Name <you@example.com>
2026-01-23 16:25:49 -0800

    Added nextcloud commands

--- nextcloud/import.sh
+++ nextcloud/import.sh
@@ -1,1 +1,3 @@
 docker exec -ti --user www-data nextcloud /var/www/html/occ files:scan --all
+docker exec -ti --user www-data nextcloud /var/www/html/occ app:install duplicatefinder
+docker exec -ti --user www-data nextcloud /var/www/html/occ duplicates:find-all
