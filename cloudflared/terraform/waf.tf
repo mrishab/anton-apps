7882eff96ba14e8441f6e632459943decf64417e
Rishab Manocha <rishab.manocha@outlook.com>
2026-07-18 19:16:32 -0700

    Updated WAF to not block traffic for rishabmanocha.com and block AI crawling, bots and crawlers from cloudville

--- cloudflared/terraform/waf.tf
+++ cloudflared/terraform/waf.tf
@@ -7,8 +7,8 @@
 
 resource "cloudflare_ruleset" "cloudville_geo_blocking" {
   zone_id     = var.cloudville_zone_id
-  name        = "Geo-blocking Rule"
-  description = "Block traffic not originating from allowed countries"
+  name        = "Geo-blocking and Bot Blocking Rule"
+  description = "Block traffic not originating from allowed countries and block indexing/crawling/AI bots"
   kind        = "zone"
   phase       = "http_request_firewall_custom"
 
@@ -18,20 +18,14 @@ resource "cloudflare_ruleset" "cloudville_geo_blocking" {
     enabled     = true
     expression  = "not ip.geoip.country in {${join(" ", formatlist("\"%s\"", var.allowed_countries))}}"
   }
-}
-
-resource "cloudflare_ruleset" "rishabmanocha_geo_blocking" {
-  zone_id     = var.rishabmanocha_zone_id
-  name        = "Geo-blocking Rule"
-  description = "Block traffic not originating from allowed countries"
-  kind        = "zone"
-  phase       = "http_request_firewall_custom"
 
   rules {
     action      = "block"
-    description = "Only allow traffic from allowed countries"
+    description = "Block indexing, crawling, AI, bots"
     enabled     = true
-    expression  = "not ip.geoip.country in {${join(" ", formatlist("\"%s\"", var.allowed_countries))}}"
+    expression  = "(cf.client.bot) or (cf.verified_bot_category in {\"AI Crawler\" \"Search Engine Crawler\" \"Aggregator\"}) or (lower(http.user_agent) contains \"bot\") or (lower(http.user_agent) contains \"crawl\") or (lower(http.user_agent) contains \"spider\") or (lower(http.user_agent) contains \"index\")"
   }
 }
+
+
 
